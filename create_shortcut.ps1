8a954bea6ff70d95eea5bd05c9a574f033792ee3
Alanlin1234 <962911377@qq.com>
2025-12-09 10:52:06 +0800

    Remove unused files: duplicated crawler, scripts, and shortcuts

--- create_shortcut.ps1
+++
@@ -1,9 +0,0 @@
-# 创建ComfyUI桌面快捷方式
-$WshShell = New-Object -ComObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut([Environment]::GetFolderPath('Desktop') + '\ComfyUI.lnk')
-$Shortcut.TargetPath = 'C:\Program Files\Python310\python.exe'
-$Shortcut.Arguments = 'main.py --lowvram'
-$Shortcut.WorkingDirectory = 'D:\ComfyUI-master'
-$Shortcut.Description = '启动ComfyUI'
-$Shortcut.Save()
-Write-Host "✅ ComfyUI快捷方式已创建到桌面"
